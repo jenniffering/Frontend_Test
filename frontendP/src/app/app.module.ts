0ee721db315217d6d4dc09df2eb56e62a417f218
Jenniffer Helena Alvarez Puello <jenniffering@outlook.com>
2018-07-31 08:11:22 -0500

    components

--- frontendP/src/app/app.module.ts
+++ frontendP/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentsComponent } from './contents/contents.component';
+import { VideosComponent } from './videos/videos.component';
+import { VideoComponent } from './video/video.component';
+import { ShowVideoComponent } from './show-video/show-video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ContentsComponent,
+    VideosComponent,
+    VideoComponent,
+    ShowVideoComponent
   ],
   imports: [
     BrowserModule
---
+++ frontendP/src/app/show-video/show-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-video',
+  templateUrl: './show-video.component.html',
+  styleUrls: ['./show-video.component.css']
+})
+export class ShowVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
